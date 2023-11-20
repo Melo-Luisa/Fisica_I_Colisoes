445103c6610ee32047d2c7e151ea5751a0da93ea
Melo-Luisa <97964206+Melo-Luisa@users.noreply.github.com>
2023-11-20 11:07:41 -0300

    criado a função de colisão

--- fisica_work/dinamica_fisica.py
+++ fisica_work/dinamica_fisica.py
@@ -20,24 +20,27 @@ class bolas:
         if self.coordX < 0 or self.coordX > largura:
             self.speedX *= -1
         elif self.coordY < 0 or self.coordY > altura:
-            self.speedY *= -1
-    def colisoes(self):
-        pass
-
-
+            self.speedY *= -1 #mudei a direção da velocidade, que soma com a coordenada 
+    def colisoes(self, other_ball):
+        if self.coordX == other_ball.coordX: #se as duas bolas estiverem na msm coordenada do eixo x
+            print("Bateu eixo x")
+        if self.coordY == other_ball.coordY:
+            print("Bateu eixo y")  
+         
 running = True
 
 
-n_bolas = 5
+n_bolas = 3
 raio = 10
 lista_bolas = []
+
 
 blue = (0,0,255)
 clock = py.time.Clock()
 for i in range(n_bolas):
     posX = random.randint(0,largura - raio)
     posY = random.randint(0,altura)
-    lista_bolas.append(bolas(posX,posY,i*0.24,5,raio))
+    lista_bolas.append(bolas(posX,posY,i*2,5,raio))
     posY += 30
     posX += 20
 
@@ -51,10 +54,14 @@ while running:
             running = False
     #screen.fill(0,0,0)
 
-    for ball in lista_bolas: 
+    for i, ball in enumerate(lista_bolas): 
         ball.move()
         ball.limite_wall()
+        for other_ball in lista_bolas[:i]:
+            ball.colisoes(other_ball)
         py.draw.circle(screen, blue, (ball.coordX, ball.coordY), ball.raio)
+        
+
 
 
     py.display.update()
